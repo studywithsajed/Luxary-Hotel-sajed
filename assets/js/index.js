5dca047f88b3586f8f94e2541c27d040cc03560a
unknown <sajedislam019@gmail.com>
2024-10-01 13:07:06 +0600

    scroll bar added

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+// toggleBar menu 
 const mobile_nav = document.querySelector(".mobile-navbar-btn");
 
 const nav_header = document.querySelector(".header-area")
@@ -7,3 +8,22 @@ const toggleNavbar = () => {
 }
 
 mobile_nav.addEventListener("click", () => toggleNavbar());
+
+// scrollBartop
+
+const headerElement = document.querySelector('.header-top-area');
+
+const footerElement = document.querySelector(".footer-area");
+
+const scrollElement = document.createElement("div");
+scrollElement.classList.add("scroll-top");
+
+scrollElement.innerHTML = `<i class="fa-solid fa-arrow-up scroll-style"></i>`;
+
+footerElement.after(scrollElement);
+
+const scrollTop = () => {
+  headerElement.scrollIntoView({
+    behavior: "smooth"
+  })
+}
